509b9f415593c5ee509df602dcf9a0501d02a2a4
gy2108 <gopalyadav2108@gmail.com>
2019-08-23 02:17:27 +0530

    Added Custom USer Model

--- app/core/models.py
+++ app/core/models.py
@@ -10,7 +10,14 @@ class UserManager(BaseUserManager):
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
+        return user
 
+    def create_superuser(self, email, password):
+        """Creates and saves a new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff=True
+        user.is_superuser=True
+        user.save(using=self._db)
         return user
 
 class User(AbstractBaseUser, PermissionsMixin):
